15504dc3cc9be82f4dfc5a449fc16ced4142842d
brandojohnson <brandon@rjohnsononline.com>
2014-12-10 22:12:40 -0600

    Deleted page-home.php and replaced with front-page.php which is the default wordpress homepage page template.

--- themes/wpbootstrap/header.php
+++ themes/wpbootstrap/header.php
@@ -32,11 +32,7 @@
 <nav id="navigation">
     <div class="container">
         <ul class="navlinks">
-            <li><a href="index.html">Homepage</a></li>
-            <li><a href="index.html">About Us</a></li>
-            <li><a href="index.html">Projects</a></li>
-            <li><a href="index.html">The Team</a></li>
-            <li><a href="index.html">Contacts</a></li>
+            <?php wp_list_pages(array('title_li' => '')); ?>
         </ul>
     </div>
 </nav>
