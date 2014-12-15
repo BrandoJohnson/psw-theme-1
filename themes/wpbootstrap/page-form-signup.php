7fa77cbbab1a2a8b6993629892c1a02585cc25e5
brandojohnson <brandon@rjohnsononline.com>
2014-12-15 17:11:03 -0600

    Grey bar atop the Sign-Up page.

--- themes/wpbootstrap/page-form-signup.php
+++ themes/wpbootstrap/page-form-signup.php
@@ -5,8 +5,9 @@ Template Name: Sign-Up Form
 */
 
 get_header(); ?>
+    <div id="footer" align="left"><h2><div class="selected">Choose Your Plan > <span class="selectedon">Pick Your Username</span> >  Create Your Site </div></h2></div>
 
-<section class="section row">
+    <section class="section row">
     <div class="section-headlines text-center span8 signform">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <div class="span8 text-left"><h2><?php the_title(); ?></h2>
