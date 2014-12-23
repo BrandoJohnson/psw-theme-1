c99931cb06f789067b55afb4a360fbc479b5669f
brandojohnson <brandon@rjohnsononline.com>
2014-12-23 15:49:11 -0600

    Fixed pricing page styling and other edits to frontpage.

--- themes/wpbootstrap/page-pricing.php
+++ themes/wpbootstrap/page-pricing.php
@@ -16,98 +16,98 @@ get_header(); ?>
                 <p><?php _e('Sorry, this page does not exist.'); ?></p>
             <?php endif; ?>
         </div>
-    <div class="main-content">
-        <div class="container row-fluid">
-            <div class="pricing-table row-fluid text-center row">
-                <div class="span4 ">
-                    <div class="plan">
-                        <div class="plan-name">
-                            <h2>One-Time Setup Fee</h2>
-                            <p class="muted">Your new site and domain name will be live quickly. <br/>We do everything for you.</p>
-                        </div>
-                        <div class="plan-price plan-setup">
-                            <b>$249</b>
-                        </div>
-                        <div class="plan-details">
-                            <div>
-                                <b>Design Customization</b>
+        <div class="main-content">
+            <div class="container row-fluid">
+                <div class="pricing-table row-fluid text-center">
+                    <div class="span4 ">
+                        <div class="plan">
+                            <div class="plan-name">
+                                <h2>One-Time Setup Fee</h2>
+                                <p class="muted">Your new site and domain name will be live quickly. <br/>We do everything for you.</p>
                             </div>
-                            <div>
-                                <b>Input of all Menus and Content</b>
+                            <div class="plan-price plan-setup">
+                                <b>$249</b>
                             </div>
-                            <div>
-                                <b>Activate your Domain Name</b>
-                            </div>
-                            <div>
-                                <b>Connect your Social Media Profiles</b>
-                            </div>
-                            <div style="height: 3.4em;">
-                                <b>Go Live on One Week!</b>
-                            </div>
+                            <div class="plan-details">
+                                <div>
+                                    <b>Design Customization</b>
+                                </div>
+                                <div>
+                                    <b>Input of all Menus and Content</b>
+                                </div>
+                                <div>
+                                    <b>Activate your Domain Name</b>
+                                </div>
+                                <div>
+                                    <b>Connect your Social Media Profiles</b>
+                                </div>
+                                <div style="height: 3.4em;">
+                                    <b>Go Live on One Week!</b>
+                                </div>
 
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="span4">
+                        <div class="plan prefered">
+                            <div class="plan-name">
+                                <h2>Service Plan</h2>
+                                <p class="muted">The Service Plan Begins after initial setup. <br/>It covers web updates, hosting and support.</p>
+                            </div>
+                            <div class="plan-price">
+                                <b>+$59</b> / month
+                            </div>
+                            <div class="plan-details">
+                                <div>
+                                    First Month is <b>Free</b>
+                                </div>
+                                <div>
+                                    <b>Reliable Hosting</b>
+                                </div>
+                                <div>
+                                    <b>24/7 Support</b>
+                                </div>
+                                <div>
+                                    <b>Content and Web Updates</b>
+                                </div>
+                            </div>
+                            <div class="plan-action">
+                                <a href="<?php get_site_url(); ?>/sign-up-2/" class="btn btn-success btn-block btn-large">Choose Plan</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="span4">
+                        <div class="plan">
+                            <div class="plan-name">
+                                <h2>Service + Marketing Plan</h2>
+                                <p class="muted">Boost your reach with an email marketing campaign - With all of the benefits of the Service Plan, with Email Marketing.</p>
+                            </div>
+                            <div class="plan-price">
+                                <b>+$99</b> / month
+                            </div>
+                            <div class="plan-details">
+                                <div>
+                                    First Month is <b>Free</b>
+                                </div>
+                                <div>
+                                    <b>Monthly Email Newsletter</b>
+                                </div>
+                                <div>
+                                    <b>Up to 2,000 Subscribers</b>
+                                </div>
+                                <div><b>Includes all the Benefits of the Service Plan</b></div>
+                            </div>
+                            <div class="plan-action">
+                                <a href="<?php get_site_url(); ?>/sign-up-3/" class="btn btn-block btn-large">Choose Plan</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div class="span4">
-                    <div class="plan prefered">
-                        <div class="plan-name">
-                            <h2>Service Plan</h2>
-                            <p class="muted">The Service Plan Begins after initial setup. <br/>It covers web updates, hosting and support.</p>
-                        </div>
-                        <div class="plan-price">
-                            <b>+$59</b> / month
-                        </div>
-                        <div class="plan-details">
-                            <div>
-                                First Month is <b>Free</b>
-                            </div>
-                            <div>
-                                <b>Reliable Hosting</b>
-                            </div>
-                            <div>
-                                <b>24/7 Support</b>
-                            </div>
-                            <div>
-                                <b>Content and Web Updates</b>
-                            </div>
-                        </div>
-                        <div class="plan-action">
-                            <a href="<?php get_site_url(); ?>/sign-up-2/" class="btn btn-success btn-block btn-large">Choose Plan</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="span4">
-                    <div class="plan">
-                        <div class="plan-name">
-                            <h2>Service + Marketing Plan</h2>
-                            <p class="muted">Boost your reach with an email marketing campaign - With all of the benefits of the Service Plan, with Email Marketing.</p>
-                        </div>
-                        <div class="plan-price">
-                            <b>+$99</b> / month
-                        </div>
-                        <div class="plan-details">
-                            <div>
-                                First Month is <b>Free</b>
-                            </div>
-                            <div>
-                                <b>Monthly Email Newsletter</b>
-                            </div>
-                            <div>
-                                <b>Up to 2,000 Subscribers</b>
-                            </div>
-                            <div><b>Includes all the Benefits of the Service Plan</b></div>
-                        </div>
-                        <div class="plan-action">
-                            <a href="<?php get_site_url(); ?>/sign-up-3/" class="btn btn-block btn-large">Choose Plan</a>
-                        </div>
-                    </div>
-                </div>
+                <p class="muted text-center">Note: You can change or cancel your plan at anytime in your account settings.</p>
             </div>
-            <p class="muted text-center">Note: You can change or cancel your plan at anytime in your account settings.</p>
         </div>
     </section>
-</div>
 
 <?php get_footer(); ?>
