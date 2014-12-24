272cd881637e4241d179143c92229c1bf33328cb
brandojohnson <brandon@rjohnsononline.com>
2014-12-24 15:31:20 -0600

    Signup form page edits.

--- themes/wpbootstrap/page-form-signup.php
+++ themes/wpbootstrap/page-form-signup.php
@@ -8,9 +8,9 @@ get_header(); ?>
 
 
 <div id="main-content">
-    <div id="topside" align="left">
+    <div id="topside" align="left" class="row">
         <h2>
-            <div class="selected"><span class="selectedon">Choose Your Plan ></span> Pick Your Username > Create Your Site
+            <div class="selected span12"><span class="selectedon">Choose Your Plan ></span> Pick Your Username > Create Your Site
             </div>
         </h2>
     </div>
