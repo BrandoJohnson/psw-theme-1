d54ed8f4720630fa77fda602b511101763fe6816
brandojohnson <brandon@rjohnsononline.com>
2014-12-24 15:27:05 -0600

    Signup form page edits.

--- themes/wpbootstrap/page-form-signup.php
+++ themes/wpbootstrap/page-form-signup.php
@@ -14,7 +14,7 @@ get_header(); ?>
 
 <div id="main-content">
 
-    <div class="container">
+    <div class="container row">
                 <div class="section-headlines signform span8">
                     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                         <div class="text-left"><h2><?php the_title(); ?></h2>
