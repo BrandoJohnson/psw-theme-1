a5d3329b2aa9727910e9bbb852525c9c2b43fc32
brandojohnson <brandon@rjohnsononline.com>
2014-12-23 22:03:56 -0600

    Added 404 page.

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+
+
+    <div id="main-content">
+        <div class="container">
+            <div class="row">
+                <div class="span8">
+
+                <h2>
+                    Sorry, this page was not found. If you believe you are revieving this message in error, please contact support by clicking the link tho the right.
+                </h2>
+
+                </div>
+                <div class="span4">
+                    <?php get_sidebar(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
